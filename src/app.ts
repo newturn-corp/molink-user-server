15ab9038ad7ce295bf7006cc9e46c97afe7a2c49
molink-huseong <huseong.lee@molink.life>
2022-03-03 01:06:11 +0900

    코드 ㅅ줭

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import http from 'http'
 import SocketServer from './SocketServer'
 import {useMiddleware} from "./Configs/MiddlewareConfig";
+import 'reflect-metadata'
 
 const app = express()
 useMiddleware(app)
