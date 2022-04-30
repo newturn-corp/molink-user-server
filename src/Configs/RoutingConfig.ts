8b1034518d8884b2650c3e870264972d7c1daef7
molink-huseong <huseong.lee@molink.life>
2022-05-01 03:50:41 +0900

    Auth Checker추가

--- src/Configs/RoutingConfig.ts
+++ src/Configs/RoutingConfig.ts
@@ -2,13 +2,16 @@ import { RoutingControllersOptions } from 'routing-controllers'
 import MainController from '../Controllers/http'
 
 import { CustomErrorHandler } from '../Middlewares/CustomErrorHandler'
+import { AuthMiddleware } from '../Middlewares/AuthMiddleware'
 
 const routingControllersOptions: RoutingControllersOptions = {
     defaultErrorHandler: false,
     middlewares: [CustomErrorHandler],
     controllers: [
         MainController
-    ]
+    ],
+    authorizationChecker: AuthMiddleware.authorization,
+    currentUserChecker: AuthMiddleware.currentUser
 }
 
 export { routingControllersOptions }
---
+++ src/Middlewares/AuthMiddleware.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken'
+import { Action, UnauthorizedError } from 'routing-controllers'
+import env from '../env'
+import { JWTUser } from '@newturn-develop/types-molink'
+import UserRepo from '../Repositories/UserRepo'
+
+export class AuthMiddleware {
+    static async authorization (action: Action) {
+        const { token } = action.request.cookies
+
+        if (!token) return false
+
+        try {
+            jwt.verify(token, env.jwt)
+            return true
+        } catch (e) {
+            return false
+        }
+    }
+
+    static async currentUser (action: Action) {
+        const { token } = action.request.cookies
+        if (!token) return false
+        try {
+            const decoded = jwt.verify(token, env.jwt) as JWTUser
+            if (!decoded.id) {
+                throw new UnauthorizedError()
+            }
+
+            const user = await UserRepo.getActiveUserById(decoded.id)
+            if (!user) {
+                throw new UnauthorizedError()
+            }
+            return user
+        } catch (e) {
+            throw new UnauthorizedError()
+        }
+    }
+}
