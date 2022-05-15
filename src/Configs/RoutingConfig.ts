29e9bf3a0a279ce4cbad9f42f50ad46198e40f4d
molink-huseong <huseong.lee@molink.life>
2022-05-16 02:59:57 +0900

    NotificationController 추가

--- src/Configs/RoutingConfig.ts
+++ src/Configs/RoutingConfig.ts
@@ -4,13 +4,15 @@ import MainController from '../Controllers/http'
 import { CustomErrorHandler } from '../Middlewares/CustomErrorHandler'
 import { AuthMiddleware } from '../Middlewares/AuthMiddleware'
 import FollowController from '../Controllers/follow'
+import NotificationController from '../Controllers/notifications'
 
 const routingControllersOptions: RoutingControllersOptions = {
     defaultErrorHandler: false,
     middlewares: [CustomErrorHandler],
     controllers: [
         MainController,
-        FollowController
+        FollowController,
+        NotificationController
     ],
     authorizationChecker: AuthMiddleware.authorization,
     currentUserChecker: AuthMiddleware.currentUser
--- src/Services/NotificationService.ts
+++ src/Services/NotificationService.ts
@@ -3,17 +3,6 @@ import User from '../Domain/User'
 import { NotificationInfo } from '@newturn-develop/types-molink/dist/Domains/NotificationInfo'
 
 class NotificationService {
-    // getNotificationMessage (causeUser: User, notification: Notification) {
-    //     switch (notification.notificationType) {
-    //     case NotificationType.NewFollow:
-    //         return `새로운 팔로워: ${causeUser.nickname}님`
-    //     case NotificationType.FollowAccept:
-    //         return `${causeUser.nickname}님이 팔로우 요청을 수락하셨습니다.`
-    //     default:
-    //         throw new Error('Unhandled Notification Type')
-    //     }
-    // }
-
     async getActiveNotifications (user: User) {
         const notifications = await NotificationRepo.getActiveNotificationsByUserId(user.id)
         return notifications.map(noti => {
