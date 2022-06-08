19df88d2c3d79cf42f718832865ac6e3bcf74e03
molink-huseong <huseong.lee@molink.life>
2022-06-09 08:04:03 +0900

    중복된 cron 제거

--- src/cron.ts
+++ src/cron.ts
@@ -13,13 +13,4 @@ export const startCron = () => {
             console.log(err)
         }
     })
-
-    schedule.scheduleJob('0 0 * * *', async () => {
-        try {
-            const stats = SynchronizationService.getServiceStats()
-            await Slack.sendTextMessage(`User Server ${ip.address()} 통계\n현재 문서의 수: ${stats.documentCount}\n전체 유저의 수: ${stats.totalUserCount}\n소켓이 가장 많이 연결된 문서의 ID: ${stats.maxUserID}\n해당 문서에 연결된 소켓의 수: ${stats.maxUserCount}`, env.isProduction ? 'C038JK24FU0' : 'C037N6N5SG6')
-        } catch (err) {
-            console.log(err)
-        }
-    })
 }
