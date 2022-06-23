523c1d7b516a1e5836d29c67b2ca8c4600f36177
molink-huseong <huseong.lee@molink.life>
2022-06-23 23:45:12 +0900

    빌드 안 되는 ㅁ누제 해결

--- src/Controllers/notifications.ts
+++ src/Controllers/notifications.ts
@@ -1,15 +1,9 @@
 import { JsonController, Authorized, Get, CurrentUser, Body, Put, Post } from 'routing-controllers'
 import User from '../Domain/User'
-import FollowService from '../Services/FollowService'
 import {
-    AcceptFollowRequestDTO, FollowRequestDTO,
     makeEmptyResponseMessage,
-    makeResponseMessage,
-    RejectFollowRequestDTO
+    makeResponseMessage
 } from '@newturn-develop/types-molink'
-import { UserNotExists } from '../Errors/ProfileError'
-import { CustomHttpError } from '../Errors/HttpError'
-import { AlreadyFollowing, AlreadyFollowRequested } from '../Errors/FollowError'
 import NotificationService from '../Services/NotificationService'
 
 @JsonController('/notifications')
